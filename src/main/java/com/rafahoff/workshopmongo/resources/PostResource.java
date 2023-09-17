0d67263125188308655254e3aecae2cad3249dc3
Rafa-Hoff <rafahoff32@gmail.com>
2023-09-17 11:21:18 -0400

    Getting a Post by id

---
+++ src/main/java/com/rafahoff/workshopmongo/resources/PostResource.java
@@ -0,0 +1,21 @@
+package com.rafahoff.workshopmongo.resources;
+
+import com.rafahoff.workshopmongo.domain.Post;
+import com.rafahoff.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) //or use @GetMapping
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
